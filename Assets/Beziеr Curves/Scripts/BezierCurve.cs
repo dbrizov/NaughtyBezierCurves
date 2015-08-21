a0314efa4cbd7f4c964e2804ceea7eca89d2d956
Denis Rizov <d.b.rizov@gmail.com>
2015-08-22 00:37:04 +0300

    Code refactoring

--- Assets/Beziеr Curves/Scripts/BezierCurve.cs
+++ Assets/Beziеr Curves/Scripts/BezierCurve.cs
@@ -7,6 +7,7 @@ namespace BezierCurves
     {
         // Serializable Fields
         [SerializeField]
+        [Tooltip("The color used to render the curve in the scene")]
         private Color color = Color.green;
 
         [SerializeField]
@@ -21,18 +22,6 @@ namespace BezierCurves
         private List<BezierPoint> points = null;
 
         // Properties
-        public Color Color
-        {
-            get
-            {
-                return this.color;
-            }
-            set
-            {
-                this.color = value;
-            }
-        }
-
         public int Precision
         {
             get
@@ -78,7 +67,7 @@ namespace BezierCurves
             // Draw the curve
             if (this.PointsCount > 1)
             {
-                Gizmos.color = this.Color;
+                Gizmos.color = this.color;
                 Vector3 fromPoint = this.Evaluate(0f);
                 for (int i = 0; i < this.antiAliasing; i++)
                 {
@@ -117,18 +106,17 @@ namespace BezierCurves
                 return this.GetPoint(this.PointsCount - 1).Position;
             }
 
-            float curveTime = 0f;
-            float totalTime = 0f;
-            float approximateLength = this.ApproximateLength;
-
             // The evaluated points is between these two points
             BezierPoint startPoint = null;
             BezierPoint endPoint = null;
+            float subCurveTime = 0f;
+            float totalTime = 0f;
+            float approximateLength = this.ApproximateLength;
 
             for (int i = 0; i < this.PointsCount - 1; i++)
             {
-                curveTime = BezierCurve.GetApproximateLengthOfCubicCurve(this.GetPoint(i), this.GetPoint(i + 1)) / approximateLength;
-                if (curveTime + totalTime > time)
+                subCurveTime = BezierCurve.GetApproximateLengthOfCubicCurve(this.GetPoint(i), this.GetPoint(i + 1)) / approximateLength;
+                if (subCurveTime + totalTime > time)
                 {
                     startPoint = this.GetPoint(i);
                     endPoint = this.GetPoint(i + 1);
@@ -136,32 +124,11 @@ namespace BezierCurves
                     break;
                 }
 
-                totalTime += curveTime;
+                totalTime += subCurveTime;
             }
 
-            float timeOfSubCurve = ((time - totalTime) / curveTime);
-            return BezierCurve.EvaluateCubicCurve(timeOfSubCurve, startPoint, endPoint);
-        }
-
-        public static float GetApproximateLengthOfCubicCurve(BezierPoint startPoint, BezierPoint endPoint, int precision = 10)
-        {
-            return GetApproximateLengthOfCubicCurve(startPoint.Position, endPoint.Position, startPoint.RightHandlePosition, endPoint.LeftHandlePosition, precision);
-        }
-
-        public static float GetApproximateLengthOfCubicCurve(Vector3 startPosition, Vector3 endPosition, Vector3 startTangent, Vector3 endTangent, int precision = 10)
-        {
-            float length = 0f;
-            Vector3 fromPoint = EvaluateCubicCurve(0f, startPosition, endPosition, startTangent, endTangent);
-
-            for (int i = 0; i < precision; i++)
-            {
-                float time = (i + 1) / (float)precision;
-                Vector3 toPoint = EvaluateCubicCurve(time, startPosition, endPosition, startTangent, endTangent);
-                length += Vector3.Distance(fromPoint, toPoint);
-                fromPoint = toPoint;
-            }
-
-            return length;
+            float timeMappedToSubCurve = ((time - totalTime) / subCurveTime);
+            return BezierCurve.EvaluateCubicCurve(timeMappedToSubCurve, startPoint, endPoint);
         }
 
         public static Vector3 EvaluateCubicCurve(float time, BezierPoint startPoint, BezierPoint endPoint)
@@ -192,6 +159,27 @@ namespace BezierCurves
             result += ttt * endPosition; // Fourth term
 
             return result;
+        }
+
+        public static float GetApproximateLengthOfCubicCurve(BezierPoint startPoint, BezierPoint endPoint, int precision = 10)
+        {
+            return GetApproximateLengthOfCubicCurve(startPoint.Position, endPoint.Position, startPoint.RightHandlePosition, endPoint.LeftHandlePosition, precision);
+        }
+
+        public static float GetApproximateLengthOfCubicCurve(Vector3 startPosition, Vector3 endPosition, Vector3 startTangent, Vector3 endTangent, int precision = 10)
+        {
+            float length = 0f;
+            Vector3 fromPoint = EvaluateCubicCurve(0f, startPosition, endPosition, startTangent, endTangent);
+
+            for (int i = 0; i < precision; i++)
+            {
+                float time = (i + 1) / (float)precision;
+                Vector3 toPoint = EvaluateCubicCurve(time, startPosition, endPosition, startTangent, endTangent);
+                length += Vector3.Distance(fromPoint, toPoint);
+                fromPoint = toPoint;
+            }
+
+            return length;
         }
     }
 }
